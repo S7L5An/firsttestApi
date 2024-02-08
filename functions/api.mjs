0cc71a3473323c5e684c5dc614558bd2d3311b26
S7L5An <152207628+S7L5An@users.noreply.github.com>
2024-02-08 16:56:05 +0500

    first uploading

---
+++ functions/api.mjs
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import ServerlessHttp from 'serverless-http';
+const app = express()
+
+const port = 9000
+
+const data = [
+    {name:'User1',surname:'John'},
+    {name:'User2',surname:'Doe'},
+
+];
+app.use(bodyParser.json())
+
+app.get('/getAllUsers',(req,res)=>{
+    res.json(data);
+})
+
+app.post('/addNewUser',(req,res)=>{
+    data.push(req.body)
+    res.send('Successfully added')
+})
+
+export const handler = ServerlessHttp(app);
